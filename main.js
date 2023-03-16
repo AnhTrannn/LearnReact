af516de24653cb9ecfade69e90439a1b933bfbc7
Ace <qanhltv11@gmail.com>
2023-03-16 16:06:58 +0700

    Main Day4

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
 const input = document.getElementById('input');
 const list = document.getElementsByClassName('list');
+
+console.log(list);
+
+const arr = [...list];
+arr.push('Hello');
+console.log(arr);
+
